8ecd20934e4b2ee4f81cec67feb54204211459be
Harshul26Nanwani <72592254+Harshul26Nanwani@users.noreply.github.com>
2022-03-20 22:39:53 +0530

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    cout<<" Enter Numbers a b c d ";
+    cin>>a>>b>>c>>d;
+    d=a+b+c;
+    cout<<"Sum= "<<d;
+    return 0;
+}
+
